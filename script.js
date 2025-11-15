5a7bde787605042c204323011dccf7f08eb228ed
labledsoe77-Gimy <labledsoe77@gmail.com>
2025-11-15 12:54:47 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('youtubeButton').addEventListener('click', function() {
+    window.location.href = 'https://www.youtube.com/watch?v=Aq5WXmQQooo&list=RDAq5WXmQQooo&start_radio=1';
+});
