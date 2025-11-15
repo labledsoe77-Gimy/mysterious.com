b9c71723a237049b32beb1fd6acdc46010fc0b06
labledsoe77-Gimy <labledsoe77@gmail.com>
2025-11-15 13:02:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-document.getElementById('youtubeButton').addEventListener('click', function() {
+// Select the button with ID 'youtubeButton'
+const button = document.getElementById('youtubeButton');
+
+// Add a click event listener
+button.addEventListener('click', () => {
+    // Redirect to the YouTube link when clicked
     window.location.href = 'https://www.youtube.com/watch?v=Aq5WXmQQooo&list=RDAq5WXmQQooo&start_radio=1';
 });
